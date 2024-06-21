4139d578344b6c59a464de3ff652dfdce7424a6a
valenpico <valenpico2005@outlook.com>
2024-06-21 19:45:32 -0300

    actualizar

--- main.cpp
+++ main.cpp
@@ -11,10 +11,10 @@ int main(){
     int asesinatos;
     int edad;
     int cantArmas;
-    int cantJugadores;
+    int n;
     cout << "ingrese la cantidad de jugadores en el torneo: " << endl;
-    cin >> cantJugadores;
-    for(int i = 0; i<cantJugadores; i++){
+    cin >> n;
+    for(int i = 0; i<n; i++){
         cout<<"ingrese el alias del jugador "<<i+1<<": ";
         cin >> alias;
         jugadores[i].alias = alias;
@@ -38,5 +38,16 @@ int main(){
         cout << "Ingrese la cantidad de armas del jugador " << i+1 << ": ";
         cin >> cantArmas;
         jugadores[i].cantArmas = cantArmas;
-    } 
+
+    }
+        
+        cout<<"----------MENU----------"<<endl;
+        cout<<"1. Encontrar el jugador con mas asesinatos."<<endl;
+        cout<<"2. Minutos de juego que tuvo el jugador que mas bajas le propinaron"<<endl;
+        cout<<"3. Porcentaje de muertes de los jugadores que jugaron entre 100 y 200 minutos"<<endl;
+        cout<<"4. Cantidad de asesinatos del jugador de mayor edad"<<endl;
+        cout<<"5. Alias jugador con menos edad"<<endl;
+        cout<<"6. Cantidad de muertes en promedio durante el juego"<<endl;
+        cout<<"7. Edad del jugador con menos asesinatos"<<endl; 
+        cout<<"la mama de mati es mia"<<endl;
 }
