7e747a7ee5da11ac07facc50e1cc4b8267919603
valenpico <valenpico2005@outlook.com>
2024-06-22 17:45:57 -0300

    acomodar funcione sy borrar pantalla

--- main.cpp
+++ main.cpp
@@ -1,20 +1,24 @@
 #include "function.h"
 #include "personaje.h"
 #include "cstring"
+#include <cstdlib>
 #include <iostream>
 using namespace std;
-int main()
+
+void clearScreen()
 {
-    Personaje jugadores[100];
+    system("cls");
+}
+
+
+void ingresarJugador(Personaje jugadores[100], int n)
+{
     string alias;
     int minutos;
     int muertes;
     int asesinatos;
     int edad;
     int cantArmas;
-    int n;
-    cout << "ingrese la cantidad de jugadores en el torneo: " << endl;
-    cin >> n;
     for (int i = 0; i < n; i++)
     {
         cout << "ingrese el alias del jugador " << i + 1 << ": ";
@@ -41,14 +45,39 @@ int main()
         cin >> cantArmas;
         jugadores[i].cantArmas = cantArmas;
     }
+}
 
-    cout << "----------MENU----------" << endl;
-    cout << "1. Encontrar el jugador con mas asesinatos." << endl;
-    cout << "2. Minutos de juego que tuvo el jugador que mas bajas le propinaron" << endl;
-    cout << "3. Porcentaje de muertes de los jugadores que jugaron entre 100 y 200 minutos" << endl;
-    cout << "4. Cantidad de asesinatos del jugador de mayor edad" << endl;
-    cout << "5. Alias jugador con menos edad" << endl;
-    cout << "6. Cantidad de muertes en promedio durante el juego" << endl;
-    cout << "7. Edad del jugador con menos asesinatos" << endl;
-    cout << "la mama de mati es mia" << endl;
+
+int main()
+{
+    Personaje jugadores[100];
+    int n;
+    int opcion;
+
+    do
+    {
+        clearScreen();
+        cout << "----------MENU----------" << endl;
+        cout << "1. Ingresar jugadores" << endl;
+        cout << "2. Encontrar el jugador con mas asesinatos." << endl;
+        cout << "3. Minutos de juego que tuvo el jugador que mas bajas le propinaron" << endl;
+        cout << "4. Porcentaje de muertes de los jugadores que jugaron entre 100 y 200 minutos" << endl;
+        cout << "5. Cantidad de asesinatos del jugador de mayor edad" << endl;
+        cout << "6. Alias jugador con menos edad" << endl;
+        cout << "7. Cantidad de muertes en promedio durante el juego" << endl;
+        cout << "8. Edad del jugador con menos asesinatos" << endl;
+        cout << "9. Salir" << endl;
+        cin>>opcion;
+        clearScreen();
+        switch (opcion)
+        {
+        case 1:
+            cout << "Ingrese la cantidad de jugadores: ";
+            cin >> n;
+            ingresarJugador(jugadores, n);
+            break;
+        }
+        
+    } while (opcion != 9 && opcion > 0);
+    
 }
